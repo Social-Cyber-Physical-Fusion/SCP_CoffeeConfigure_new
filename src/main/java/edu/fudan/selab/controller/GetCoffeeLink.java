1d7a0a77f0b5bcf41016dbb8d742afe1a76c442e
HeFeijia <okjjhfj111@126.com>
2020-03-06 14:04:09 +0800

    add instance_id

--- src/main/java/edu/fudan/selab/controller/GetCoffeeLink.java
+++ src/main/java/edu/fudan/selab/controller/GetCoffeeLink.java
@@ -9,12 +9,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import static edu.fudan.selab.globle.Constants.*;
 
@@ -28,11 +24,12 @@ public class GetCoffeeLink {
 
         String taskId = request.getParameter("processIdFromApp");
         workflow_instance_id = request.getParameter("workflow_instance_id");
-
+        instance_id = request.getParameter("instance_id");
         userId = request.getParameter("userId");
 //        System.out.println(userId+"__________________________________________________________________");
         String RedirectApp = SC_url + "/task/saveTaskNodeMessage";
 //        String notifyApp = App_url + "/user/sendMessageToMPOne";
+        String FeedBack =  process_proxy_url + "/save_action_operation_self";
 
         Map<String,String> map = new HashMap<String,String>();
         map.put("userId",userId);
@@ -40,6 +37,7 @@ public class GetCoffeeLink {
         map.put("nodeId","Human_Machine_Thing-1_PerformSelectCoffeeService");
         map.put("url",link);
         map.put("content","Please select the type of Coffee!");
+        map.put("instance_id", instance_id);
 
         new Thread() {
             public void run() {
@@ -51,21 +49,20 @@ public class GetCoffeeLink {
             }
         }.start();
 
-//        Map<String,String> map1 = new HashMap<String,String>();
-//        map1.put("userId",userId);
-//        map1.put("content","You have a task of selecting coffee to continue!");
-//        map1.put("url","www.baidu.com");
-//        new Thread() {
-//            public void run() {
-//                try {
-//                    String res = new HttpRequestor().doPost(RedirectApp, map);
-//                    String res1 = new HttpRequestor().doPost(notifyApp, map1);
-//                } catch (Exception e) {
-//                    e.printStackTrace();
-//                }
-//                //String res1 = new HttpRequestor().doPost(notifyApp,map1);
-//            }
-//        }.start();
+        Map<String,String> map1 = new HashMap<String,String>();
+        map1.put("url", link);
+        map1.put("content","Please select the type of Coffee!");
+        map1.put("instance_id", instance_id);
+
+        new Thread() {
+            public void run() {
+                try {
+                    String res1 = new HttpRequestor().doPost(FeedBack, map1);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();
 
 
         return link;
@@ -73,32 +70,18 @@ public class GetCoffeeLink {
 
     @RequestMapping(value = "/getcoffeelink", method = RequestMethod.POST)
     public String getLinkbyPOST(HttpServletRequest request) throws Exception{
-        String uri = "http://127.0.0.1:8002/s/getcoffeelink";//
-        String link = "";
-        String regex = "(?<=://)[a-zA-Z\\.0-9]+(:[0-9]+)?(?=\\/)";//"(?<=\\\\/)[a-zA-Z\\\\.:0-9]+(?=\\\\/)"; // 匹配两个斜杠之间的字符串 /.../
-
-        uri = uri.replaceAll(regex, "");
-        uri = uri.replaceAll("http(s)?://", "");
-        uri = uri.replaceAll("/getcoffeelink", "");
-        String[] split_url = uri.split("\\/");
-        List<String> list = Arrays.asList(split_url);
-
-        if(split_url.length > 0){
-            link = local_url + String.join("/", list) +"/coffee";
-        }else{
-            link = local_url + "/" + war_name +"/coffee";
-        }
-
+        String link = local_url + "/coffee";
         //String node_id = "";
         //url_back = request.getParameter("callbackUrl");
 
         String taskId = request.getParameter("processIdFromApp");
         workflow_instance_id = request.getParameter("workflow_instance_id");
-
+        instance_id = request.getParameter("instance_id");
         userId = request.getParameter("userId");
 //        System.out.println(userId+"__________________________________________________________________");
         String RedirectApp = SC_url + "/task/saveTaskNodeMessage";
         String notifyApp = SC_url + "/user/sendMessageToMPOne";
+        String FeedBack =  process_proxy_url + "/save_action_operation_self";
 
         Map<String,String> map = new HashMap<String,String>();
         map.put("userId", userId);
@@ -106,6 +89,7 @@ public class GetCoffeeLink {
         map.put("nodeId","Human_Machine_Thing-1_PerformSelectCoffeeService");
         map.put("url",link);
         map.put("content","Please select the type of Coffee!");
+        map.put("instance_id", instance_id);
 
         new Thread() {
             public void run() {
@@ -117,21 +101,20 @@ public class GetCoffeeLink {
             }
         }.start();
 
-//        Map<String,String> map1 = new HashMap<String,String>();
-//        map1.put("userId",userId);
-//        map1.put("content","You have a task of selecting coffee to continue!");
-//        map1.put("url","www.baidu.com");
-//        new Thread() {
-//            public void run() {
-//                try {
-//                    String res = new HttpRequestor().doPost(RedirectApp, map);
-//                    String res1 = new HttpRequestor().doPost(notifyApp, map1);
-//                } catch (Exception e) {
-//                    e.printStackTrace();
-//                }
-//                //String res1 = new HttpRequestor().doPost(notifyApp,map1);
-//            }
-//        }.start();
+        Map<String,String> map1 = new HashMap<String,String>();
+        map1.put("url", link);
+        map1.put("content","Please select the type of Coffee!");
+        map1.put("instance_id", instance_id);
+
+        new Thread() {
+            public void run() {
+                try {
+                    String res1 = new HttpRequestor().doPost(FeedBack, map1);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();
 
 
         return link;
--- src/main/java/edu/fudan/selab/globle/Constants.java
+++ src/main/java/edu/fudan/selab/globle/Constants.java
@@ -2,13 +2,15 @@ package edu.fudan.selab.globle;
 
 public class Constants {
     //public static final String broker_url = "http://192.168.1.102:8082";
-    public static final String local_url = "http://106.15.102.123:32766"; // 下单服务地址（即本地地址）
+    public static final String local_url = "http://139.196.228.210:32766"; // 下单服务地址（即本地地址）
     //public static final String App_url = "http://192.168.1.168:8080";
     public static final String SC_url = "http://139.224.128.113:8888"; // 微信发消息接口地址
+    public static final String process_proxy_url = "http://106.15.102.123:5001"; // k8s 流程代理服务地址
     public static final String Proxy_url = "http://ordercoffee-service-proxy.default:8888/receive_result";// 代理地址
 
     public static String url_back = "";
     public static String userId = "";
     public static String workflow_instance_id = "";
-    public static String war_name = "CoffeeConfigure";
+
+    public static String instance_id = "";
 }
